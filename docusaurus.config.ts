311dc139e6f5d2091499424e54c016af8f71f5d6
Eric Xu <ericxu2048@gmail.com>
2024-02-26 22:00:19 -0500

    Docusaurus config + cubing.js

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,20 +3,20 @@ import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
-  title: "My Site",
-  tagline: "Dinosaurs are cool",
+  title: "Roux Method",
+  tagline: "Learn the Roux method for solving the Rubik's Cube quickly!",
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://www.rouxmethod.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "err0rcuber", // Usually your GitHub org/user name.
+  projectName: "roux-method-docs", // Usually your repo name.
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
@@ -35,17 +35,13 @@ const config: Config = {
       {
         docs: {
           sidebarPath: "./sidebars.ts",
-          // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/ericx20/roux-method-docs/tree/main/",
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/ericx20/roux-method-docs/tree/main/",
         },
         theme: {
           customCss: "./src/css/custom.css",
@@ -54,11 +50,12 @@ const config: Config = {
     ],
   ],
 
+  // TODO: make graphics and stuff for Roux method
   themeConfig: {
     // Replace with your project's social card
     image: "img/docusaurus-social-card.jpg",
     navbar: {
-      title: "My Site",
+      title: "Roux Method",
       logo: {
         alt: "My Site Logo",
         src: "img/logo.svg",
@@ -72,7 +69,7 @@ const config: Config = {
         },
         { to: "/blog", label: "Blog", position: "left" },
         {
-          href: "https://github.com/facebook/docusaurus",
+          href: "https://github.com/ericx20/roux-method-docs",
           label: "GitHub",
           position: "right",
         },
@@ -94,16 +91,8 @@ const config: Config = {
           title: "Community",
           items: [
             {
-              label: "Stack Overflow",
-              href: "https://stackoverflow.com/questions/tagged/docusaurus",
-            },
-            {
               label: "Discord",
-              href: "https://discordapp.com/invite/docusaurus",
-            },
-            {
-              label: "Twitter",
-              href: "https://twitter.com/docusaurus",
+              href: "https://discord.gg/NzeFnty94K",
             },
           ],
         },
@@ -116,12 +105,12 @@ const config: Config = {
             },
             {
               label: "GitHub",
-              href: "https://github.com/facebook/docusaurus",
+              href: "https://github.com/ericx20/roux-method-docs",
             },
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} err0rcuber. Powered by cubing.js and Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,
---
+++ src/components/TwistyPlayer/index.tsx
@@ -0,0 +1,93 @@
+// taken from
+// https://github.com/cubing/react-cubing/blob/main/src/TwistyPlayer/index.tsx
+import {
+  useState,
+  useEffect,
+  forwardRef,
+  useRef,
+  useImperativeHandle,
+} from "react";
+import { TwistyPlayer as TP, TwistyPlayerConfig } from "cubing/twisty";
+
+export interface TwistyPlayerExtendedConfig extends TwistyPlayerConfig {
+  className?: string;
+  rootClassName?: string;
+  onTwistyInit?: (twisty: TP) => void;
+}
+
+/**
+ * A React wrapper for the cubing.js `<twisty-player>` element.
+ */
+const TwistyPlayer = forwardRef(
+  (
+    {
+      className,
+      rootClassName,
+      onTwistyInit,
+      ...props
+    }: TwistyPlayerExtendedConfig,
+    ref
+  ) => {
+    const [twistyPlayer, setTwisty] = useState<TP>();
+    const spanRef = useRef<HTMLSpanElement | null>(null);
+
+    useEffect(() => {
+      const newTwisty = new TP(props);
+      if (className) {
+        newTwisty.className = className;
+      }
+
+      newTwisty.style.maxWidth = "100%";
+      setTwisty(newTwisty);
+      spanRef.current?.appendChild(newTwisty);
+      if (onTwistyInit) onTwistyInit(newTwisty);
+      return () => {
+        spanRef.current?.removeChild(newTwisty);
+      };
+    }, [props.alg]);
+
+    useImperativeHandle(ref, () => {
+      return twistyPlayer;
+    });
+
+    return <span className={rootClassName} ref={spanRef} />;
+  }
+);
+
+export default TwistyPlayer;
+
+/* Documentation for the experimentalStickeringMaskOrbits prop/attribute:
+This prop is used to customize the way pieces are highlighted on the cube,
+if it's beyond the default supported stickerings such as OCLL or EOCross.
+It's an experimental feature that is subject to change.
+
+Pass in a string in the format "EDGES:xxxxxxxxxxxx,CORNERS:xxxxxxxx,CENTERS:xxxxxx"
+where each "x" is one of the following characters:
+"-": regular
+"D": dim
+"I": ignored
+"X": invisible
+"O": ignore non-primary (ignore the non-primary stickers)
+"P": permute non-primary (dim the primary stickers, useful for PLL and other stickerings where just the permutation of pieces matters)
+"o": ignoriented (same as O, except the primary sticker is also dimmed)
+"?": orientation without permutation (useful for EO and CO on the entire cube! Primary stickers are replaced with turquoise)
+This is defined in https://github.com/cubing/cubing.js/blob/36c57c2dadc889b1321d05d8cfc005cef8a9bffd/src/cubing/twisty/model/props/puzzle/display/parseSerializedStickeringMask.ts
+
+"Primary stickers" are stickers that belong on the U and D faces, and FL, FR, BL, BR positions.
+They're relevant for edge orientation and corner orientation.
+
+Here is the order of the pieces for 3x3:
+EDGES:
+  UF, UR, UB, UL,
+  DF, DR, DB, DL,
+  FR, FL, BR, BL
+
+CORNERS:
+  UFR, UBR, UBL, UFL,
+  DFR, DFL, DBL, DBR
+
+CENTERS: U, L, F, R, B, D
+
+Example stickering for Roux FB:
+  EDGES:IIIIIII-I-I-,CORNERS:IIIII--I,CENTERS:I-IIII
+*/
